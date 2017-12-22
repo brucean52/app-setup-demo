c3de466bb0dff501a28acc4d9278d4faadf5b83a
brucean52 <bruce.an52@gmail.com>
2017-12-22 14:11:10 -0800

    setup api stuff complete

--- client/src/components/get_data.js
+++ client/src/components/get_data.js
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class GetData extends Component {
+
+    componentDidMount(){
+        this.getData();
+        this.getBetterStuff();
+    }
+
+    async getData(){
+        const response = await axios.get('/api/get-stuff');
+
+        console.log('Response', response);
+    }
+
+    async getBetterStuff(){
+        const response = await axios.get('/api/better-stuff');
+
+        console.log('Better Response', response);
+    }
+
     render(){
         return(
             <div> 
